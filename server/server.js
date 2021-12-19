b27c456702507ecc44e4822604b361a2aaa63d16
Praveen Kumar <praveenscience@gmail.com>
2021-12-19 12:11:02 +0000

    Basic routing of the Backend API done.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+app.get("/", (req, res) => {
+  res.json("Hello!");
+});
+app.get("/api/users/", (req, res) => {
+  res.json("Current Logged In User");
+});
+
 app.listen(port, () => {
   console.log("Server started in port " + port + ".");
 });
